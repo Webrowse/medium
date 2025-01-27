d52164f4540495c9d35c69ba205be3b7fa2cf414
Webrowse <great.adarsh@gmail.com>
2025-01-28 03:34:19 +0530

    created an NPM package for zod

--- commons/src/index.ts
+++ commons/src/index.ts
@@ -1,27 +1,28 @@
 import { z } from 'zod';
 
 export const signupInput = z.object({
-    username : z.string().email(),
+    email : z.string().email(),
     password: z.string().min(8),
     name: z.string().optional()
-
 })
-
-//type inference in zod 
-
-export type SignupInput = z.infer<typeof signupInput>
 
 export const signinInput = z.object({
-    username : z.string().email(),
-    password: z.string().min(8),
-
+    email : z.string().email(),
+    password: z.string().min(8)
 })
-
-export type SigninInput = z.infer<typeof signinInput>
 
 export const createBlogInput = z.object({
     title: z.string(),
     content: z.string()
 })
 
+export const updateBlogInput = z.object({
+    title: z.string(),
+    content: z.string(),
+    id: z.string()
+})
+
+export type SignupInput = z.infer<typeof signupInput>
+export type SigninInput = z.infer<typeof signinInput>
 export type CreateBlogInput = z.infer<typeof createBlogInput>
+export type UpdateBlogInput = z.infer<typeof updateBlogInput>
