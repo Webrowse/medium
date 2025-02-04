32c95e9652ddba535871e1513e38bd186b78d069
Webrowse <great.adarsh@gmail.com>
2025-02-04 23:37:16 +0530

    Sketeton od component in signup

---
+++ frontend/src/components/SignUpField.tsx
@@ -0,0 +1,6 @@
+
+export const SignUpField = () => {
+    return <div>
+        SignUp Field
+    </div>
+}
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,12 +1,12 @@
 import { Quote } from "../components/Quote"
-
+import { SignUpField } from "../components/SignUpField"
 
 export const Signup = () => {
     return <div>
         <div className="grid grid-cols-2 ">
         
             <div> 
-                SignUp 
+                <SignUpField /> 
             </div>
             <div className="invisible md:visible">
                 <Quote />
