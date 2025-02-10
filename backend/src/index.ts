3981b3d642761650d909c3141d7dc5baabb428fc
Webrowse <great.adarsh@gmail.com>
2025-02-10 20:38:50 +0530

    fixed backend to accomodate CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { userRouter } from "./routes/user";
 import { blogRouter } from "./routes/blog";
+import { cors } from 'hono/cors'
 // Connect to your Prisma Client
 
 type Bindings = {
@@ -8,6 +9,7 @@ type Bindings = {
   JWT_SECRET: string;
 }
 const app = new Hono<{Bindings: Bindings}>();
+app.use('/api/*', cors())
 
 //Sign up route creates an entry and returns a JWT
 app.route("/api/v1/user", userRouter);
