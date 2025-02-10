8676c74b89274a6434b569de00a7a7c1db9aa17f
Webrowse <great.adarsh@gmail.com>
2025-02-10 20:40:19 +0530

    signin/signup page functioning, backend connected

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,23 +1,73 @@
 import { SignupInput } from "@adarsh23romy/blog-commons";
-import { useState } from "react";
-import { Link } from "react-router-dom"
+import { useState, ChangeEvent } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
-export const Auth = ({type}: {type: "signup" | "signin"}) => {
-    const postInputs = useState<SignupInput>({
+export const Auth = ({ type }: { type: "signup" | "signin" }) => {
+    const navigate = useNavigate();
+    const [postInputs, setPostInputs] = useState<SignupInput>({
         email: "",
         password: "",
         name: ""
-    })
+    });
+
+    async function sendRequest() {
+        try {
+            const response = await axios.post(
+                `${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`,
+                postInputs,
+                {
+                    headers: { "Content-Type": "application/json" }
+                });
+            const jwt = response.data;
+            localStorage.setItem("token", jwt);
+            navigate("/blogs")
+        } catch (e) { console.log("error during request", e); }
+
+    }
+
     return <div className=" h-screen flex justify-center flex-col">
-        <div className="flex justify-center">
+        <div className="flex justify-center px-5">
             <div>
-                <div className="text-4xl font-extrabold">
-                    Create an account
+
+
+                <div className="px-10">
+                    {/* {JSON.stringify(postInputs)} */}
+                    <div className="text-4xl font-extrabold">
+                        {type === "signup" ? "Create an account" : "Sign-in an account"}
+                    </div>
+                    <div className="text-slate-500">
+                        {type === "signup" ? "Already have an account?" : "Don't have an account?"}
+                        <Link to={type === "signup" ? "/signin" : "/signup"} className="pl-2 underline">
+                            {type === "signup" ? "Login" : "Signup"}
+                        </Link>
+                    </div>
                 </div>
-                <div className="text-slate-400">
-                    Already have an account? 
-                    <Link to={"/signin"} className="pl-2 underline">Login</Link>
+                <div className="pt-2">
+                    {type === "signup" ? <LabelledInput label="Name" placeholder="Adarsh..." onChange={(e) => {
+                        setPostInputs({
+                            ...postInputs,
+                            name: e.target.value
+                        })
+                    }} /> : null}
+                    <LabelledInput label="Email" placeholder="abc@xyz.com" onChange={(e) => {
+                        setPostInputs({
+                            ...postInputs,
+                            email: e.target.value
+                        })
+                    }} />
+                    <LabelledInput label="Password" type={"password"} placeholder="Enter the Password" onChange={(e) => {
+                        setPostInputs({
+                            ...postInputs,
+                            password: e.target.value
+                        })
+                    }} />
+
                 </div>
+                <button onClick={sendRequest} type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4
+                     focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800
+                      dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign up" : "Sign in"}</button>
             </div>
         </div>
     </div>
@@ -29,13 +79,16 @@ interface LabelledInputType {
     // name: string;
     // value?: string;
     onChange?: (e: React.FormEvent<HTMLInputElement>) => void;
+    type?: string;
 }
 
-function LabelledInput({ label, placeholder, onChange}: LabelledInputType) {
+function LabelledInput({ label, placeholder, onChange, type = "text" }: LabelledInputType) {
     return <div>
         <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
-            <input onChange={onChange} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} required />
+            <label className="block mb-1 text-sm text-black font-bold pt-2">{label}</label>
+            <input onChange={onChange} type={type || "text"} id="first_name" className="bg-gray-50 border 
+            border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 
+            focus:border-blue-500 block w-full p-2.5 mb-3.5" placeholder={placeholder} required />
         </div>
     </div>
 }
---
+++ frontend/src/config.ts
@@ -0,0 +1,4 @@
+export const BACKEND_URL = "https://backend.webrowse.workers.dev"
+
+
+// export const BACKEND_URL = "http://localhost:8787"
