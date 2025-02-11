3fa669573e9b16e1411fa803aba3a0df4a6bbbea
Webrowse <great.adarsh@gmail.com>
2025-02-12 03:44:56 +0530

    handles undefined blog scenario

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,5 +1,26 @@
 
 
+
+import { useParams } from "react-router-dom";
+import { FullBlog } from "../components/FullBlog"
+import { useBlog } from "../hooks";
+
 export const Blog = () => {
-    return <div>Blog</div>
+    const { id } = useParams<{ id: string }>();
+    const { loading, blog } = useBlog({id:id??""});
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+    return <div>
+        
+        <div className="flex justify-center">
+            <div>
+                {blog? <FullBlog blog={blog}/> : <div>Blog not found</div>}
+                
+                
+
+            </div>
+        </div>
+    </div>
 }
